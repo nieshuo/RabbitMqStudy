409e9016e0c370b5e6fbed753dd0f70448213c44
nieshuo <694891769@qq.com>
2024-11-27 16:32:43 +0800

    1丶完成基础的rabbitmq的学习

---
+++ RabbitMqStudy/BasicRecevier/Recevier.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost", //主机地址
+    VirtualHost = "admin_vhost",    //虚拟机名
+    Port = 5672,    //端口号
+    UserName = "guest", //用户名
+    Password = "guest"  //密码
+};
+
+using (var connection = await factory.CreateConnectionAsync())
+{
+    using (var channel = await connection.CreateChannelAsync())
+    {
+        await channel.QueueDeclareAsync(
+            queue: "hello", //信道
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+
+            Thread.Sleep(2000);
+
+            channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);//手动处理消息
+
+            return Task.CompletedTask;
+        };
+        await channel.BasicConsumeAsync("hello", autoAck: false, consumer: consumer);
+        // await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);//自动处理消息
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMqStudy/BasicSender/Sender.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { 
+    HostName = "localhost", //主机地址
+    VirtualHost = "admin_vhost",    //虚拟机名,默认"/"
+    Port = 5672,    //端口号,默认5672
+    UserName = "guest", //用户名
+    Password = "guest"  //密码
+};
+using (var connection = await factory.CreateConnectionAsync())
+{
+    using (var channel = await connection.CreateChannelAsync())
+    {
+        await channel.QueueDeclareAsync(
+            queue: "hello", //信道
+            durable: false, //持久化
+            exclusive: false,   //排他
+            autoDelete: false,  //自动删除
+            arguments: null
+        );
+
+        Console.WriteLine("Please input your message,with end:");
+
+        string message = Console.ReadLine() ?? "hello";
+
+        while (message != "EXIT")
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = new BasicProperties
+            {
+                Persistent = true,//持久化的
+            };
+
+            await channel.BasicPublishAsync(
+                exchange: string.Empty, //交换机
+                routingKey: "hello", //路由
+                mandatory: true, //强制性的
+                basicProperties: properties,
+                body: body
+            );
+
+            Console.WriteLine($" [x] Sent {message}");
+            Console.WriteLine("Please input your message,with end:");
+
+            message = Console.ReadLine() ?? "hello";
+        }
+    }
+}
+    
